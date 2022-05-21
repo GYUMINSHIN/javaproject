e0ed3f465a497772e76b0e97f0470409aee169f2
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-05-21 15:48:11 +0900

    Update CardGenerator.java

--- src/CardGenerator.java
+++ src/CardGenerator.java
@@ -1,4 +1,19 @@
+import java.util.*;
 
-public class CardGenerator {
+public class CardGenerator{
 
+	static int[] symbol = {1,2,3,4};
+	static int[] number = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+	public int genSymbol() {
+		return (int)((Math.random()*10000)%4+1);
+	}
+	
+	public int genNumber() {
+		return (int)((Math.random()*10000)%13+1);
+	}
+	
+	public Card genCard() {
+		return new Card(genSymbol(),genNumber());
+	}
 }
